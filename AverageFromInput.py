0923a537fb3bd664c1b85cf89ea1f99dc7ee64b9
bryankahl <104003253+bryankahl@users.noreply.github.com>
2023-03-06 12:25:52 -0500

    Add files via upload

---
+++ AverageFromInput.py
@@ -0,0 +1,30 @@
+#CS175L
+#Bryan Kahl
+#03/02/3023
+
+def main():
+    numberfile = open('numbers.txt', 'r')
+
+    content = numberfile.readlines()
+
+
+
+    total = 0
+
+    
+    
+    n = 0
+    for number in content:
+        n += 1
+        print("I read in", n, " number(s) Current number is: ",str(n), end= '' )
+        print(' number(s) Current number is:' , float(number), end='')
+        total += float(number)
+        print(' Total is: ',total)
+
+    average = total/n
+    print("Average is: ",average)
+    
+main()
+
+
+
