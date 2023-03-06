e12d89cd15488b9582869a5fad3d3aff7bf8c62a
bryankahl <104003253+bryankahl@users.noreply.github.com>
2023-03-06 13:28:09 -0500

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,39 +1,31 @@
 #CS175L
 #Bryan Kahl
-#03/06/2023
+#03/02/2023
 
 def main():
+    numberfile = open('numbers.txt', 'r')
 
-    try:
-        
-        numberfile = open('numbers.txt', 'r')
-
-        content = numberfile.readlines()
+    content = numberfile.readlines()
 
 
 
-        total = 0
+    total = 0
 
-        
-        
-        n = 0
-        for number in content:
-            n += 1
-            print("I read in", n, " number(s) Current number is: ",str(n), end= '' )
-            print(' number(s) Current number is:' , float(number), end='')
-            total += float(number)
-            print(' Total is: ',total)
 
-        average = total/n
-        print("Average is: ",average)
 
-    except IOError:
-        print('Please make sure the file exists')
+    n = 0
+    for number in content:
+        n += 1
+        print("I read in", n, " number(s) Current number is: ",str(n), end= '' )
+        print(' number(s) Current number is:' , float(number), end='')
+        total += float(number)
+        print(' Total is: ',total)
 
-    except ValueError:
-        print('Data in file might contain a string or a non-integer chracter')
-    
+    average = total/n
+    print("Average is: ",average)
+
 main()
+
 
 
 
